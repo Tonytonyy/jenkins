8c928d95664d95bc4ff416f7716f41fac5b9b633
大麦 <damai@qunhemail.com>
2018-07-20 12:03:42 +0800

    1

--- src/main/java/springboot/web/HelloWorldController.java
+++ src/main/java/springboot/web/HelloWorldController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Spring Boot HelloWorld 案例
  *
- * Created by bysocket on 16/4/26.
  */
 @RestController
 public class HelloWorldController {
@@ -20,5 +18,11 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String sayHello() {
         return "Hello,World!";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello,damai!";
+
     }
 }
